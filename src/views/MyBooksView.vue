9991a185707266a58fe3b41afe0dceda3e81301a
vladpostu <vladpostu@gmail.com>
2022-07-02 20:36:47 +0200

    tests with firestore

--- src/views/MyBooksView.vue
+++ src/views/MyBooksView.vue
@@ -1,23 +1,41 @@
 <template>
   <div class="about">
     <div>{{ this.currentUser.displayName }}'s bookshelf</div>
-    <BookComponent title="ciao" />
+    <BookComponent
+      v-for="(book, i) in this.books"
+      :key="i"
+      :title="book.name"
+    />
   </div>
 </template>
 
 <script>
 import BookComponent from "../components/BookComponent.vue";
-// import { collection, getDocs, query, where } from "firebase/firestore";
-// import { db } from "./../App.vue";
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "./../App.vue";
 
 export default {
   name: "MyBooksView",
   data() {
     return {
       currentUser: this.$store.getters.currentUser,
+      books: [],
     };
   },
-  methods: {},
+  async beforeMount() {
+    await this.retriveMyBooks();
+    console.log(this.books);
+  },
+  methods: {
+    async retriveMyBooks() {
+      const q = query(collection(db, "books"));
+
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        this.books.push(doc.data());
+      });
+    },
+  },
   components: {
     BookComponent,
   },
